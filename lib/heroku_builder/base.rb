01fcb3056308464458b7f60c01a6bbc68dcc3cba
Jason <=>
2015-10-14 09:19:17 -0600

    exit if key is not set

--- lib/heroku_builder/base.rb
+++ lib/heroku_builder/base.rb
@@ -1,6 +1,9 @@
 module HerokuBuilder
   class Base
     def conn
+      unless ENV['HEROKU_API_KEY']
+        fail 'No Heroku API Key found, please set it using the HEROKU_API_KEY environment variable'
+      end
       @conn ||= ::PlatformAPI.connect_oauth(ENV['HEROKU_API_KEY'])
     end
   end
